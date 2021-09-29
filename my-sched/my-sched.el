0442575aaef0e55961e60c7d7866b5a95bedc012
alexgiorev <alex.giorev@gmail.com>
2021-09-29 22:39:46 +0300

    my-sched-schedule

--- my-sched/my-sched.el
+++ my-sched/my-sched.el
@@ -58,22 +58,35 @@ the entry having EID as its id."
 ;; ----------------------------------------
 ;; Scheduling
 
-(defun my-sched--get-sched-create (eid)
-  "Return the scheduling record for the entry having id EID. If a record under
-that id doesn't exist, create one, attach it to the scheduling data and return
-it. This function does not flush the data."
-  (let ((sched (seq-find (lambda (sched) (equal eid (plist-get sched :id)))
-                         my-sched--data)))
-    (unless sched
-      (setq sched `(:id ,eid :due nil :interval nil))
-      (add-to-list 'my-sched--data sched :append))
+(defun my-sched--get-sched (eid)
+  "Return the scheduling record for the entry having id EID. If there is no
+scheduling data for that EID, return nil. If EID is nil, return nil."
+  (when eid
+    (seq-find (lambda (sched) (equal eid (plist-get sched :id)))
+              my-sched--data)))
+
+(defun my-sched--create-sched (eid)
+  "Create a new scheduling record and insert into the `my-sched--data'. Assumes
+that there is no record under EID."
+  (let ((sched (list :id eid :due nil :interval nil)))
+    (add-to-list 'my-sched--data sched :append)
     sched))
 
-(defun my-sched--schedule (eid interval)
-  "Schedule the entry at EID for INTERVAL days into the future. Assumes
-(integerp INTERVAL)"
-  (let* ((sched (my-sched--get-sched-create eid))
-         (interval (max 0 interval))
+(defun my-sched--get-sched-create (eid)
+  "A facility function. Return the scheduling record for the entry having id
+EID. If a record under that id doesn't exist, create one, attach it to the
+scheduling data and return it."
+  (unless eid
+    (error "EID cannot be nil"))
+  (let ((sched (my-sched--get-sched eid)))
+    (unless sched
+      (setq sched (my-sched--create-sched eid)))
+    sched))
+    
+(defun my-sched--schedule (sched interval)
+  "Schedule SCHED for INTERVAL days from today. Assumes that SCHED is already in
+the data. Flushes the data."
+  (let* ((interval (max 0 interval))
          (day (+ (my-sched--today) interval)))
     (plist-put sched :due day)
     (plist-put sched :interval interval)
@@ -87,56 +100,96 @@ it. This function does not flush the data."
      my-sched--data)))
 
 ;; ----------------------------------------
-;; commands
+;; * commands
 
 (defun my-sched-schedule nil
   "Ask the user for a number and schedule the entry at point for that many days
 into the future"
   (interactive)
-  TODO)
+  (let* ((eid (org-id-get))
+         (sched (my-sched--get-sched eid))
+         (prompt (if sched
+                     (format "Interval (last was %s): "
+                             (plist-get sched :interval))
+                   "Interval: "))
+         (new-interval (read-number prompt)))
+    (unless sched
+      (setq eid (org-id-get-create))
+      (setq sched (my-sched--create-sched eid)))
+    (my-sched--schedule sched new-interval)))
+
+;; ** ring commands
 
 (defvar my-sched-ring nil
   "A ring of entry ids.
 Typically holds the ids of due entries so that the user can read what is due for
 today.")
 
-(defun my-sched-ring-init nil
+(defun my-sched-ring-reset nil
   (interactive)
   (let ((ids (mapcar (lambda (sched) (plist-get sched :id))
                      (my-sched--due-today))))
-    (when ids
-      (my-circlist-make ids))
-    (setq my-sched-ring ids)))
+    (if ids
+        (setq my-sched-ring (my-circlist-make ids))
+      (setq my-sched-ring nil)))
+  (when (called-interactively-p 'interactive)
+    (message "Ring reset")))
 
 (defun my-sched-ring--check nil
   (if (not my-sched-ring)
-      (user-error "Scheduling ring is empty")))
-  
-(defun my-sched-ring-goto-current nil
+      (user-error "Ring is empty")))
+
+(defun my-sched-ring-jump nil
   (interactive)
   (my-sched-ring--check)
   (org-id-open (car my-sched-ring) nil))
 
-(defun my-sched-ring-goto-next nil
+(defun my-sched-ring-jump nil
+  (interactive)
+  (let (did-open)
+  (while (and my-sched-ring (not did-open))
+    (ignore-errors
+      (org-id-open (car my-sched-ring) nil)
+      (setq did-open t))
+    (unless did-open
+      (my-circlist-pop 'my-sched-ring)))
+  (unless did-open
+    (my-sched-ring--check))))
+
+(defun my-sched-ring-next nil
   (interactive)
   (my-sched-ring--check)
   (setq my-sched-ring (cdr my-sched-ring))
-  (my-sched-ring-goto-current))
+  (my-sched-ring-jump))
 
-(defun my-sched-ring-goto-prev nil
+(defun my-sched-ring-prev nil
   (interactive)
   (my-sched-ring--check)
   (setq my-sched-ring (my-circlist-prev my-sched-ring))
-  (my-sched-ring-goto-current))
+  (my-sched-ring-jump))
 
-(defun my-sched-ring-remove-current nil
+(defun my-sched-ring-pop nil
   (interactive)
-  (let ((prev (my-circlist-prev my-sched-ring)))
-    (if (eq prev my-sched-ring)
-        (setq my-sched-ring nil)
-      (setcdr prev (cdr my-sched-ring))
-      (setq my-sched-ring (cdr prev)))))
+  (my-sched-ring--check)
+  (my-circlist-pop 'my-sched-ring)
+  (when (called-interactively-p 'interactive)
+    (message "Popped link")))
+
+;;----------------------------------------
+;; keymap
+
+(defvar my-sched-map (make-sparse-keymap))
+(progn
+  (define-key my-sched-map "r" 'my-sched-ring-reset)
+  (define-key my-sched-map "p" 'my-sched-ring-prev)
+  (define-key my-sched-map "n" 'my-sched-ring-next)
+  (define-key my-sched-map "j" 'my-sched-ring-jump)
+  (define-key my-sched-map "o" 'my-sched-ring-pop)
+  (define-key my-sched-map "s" 'my-sched-schedule))
+
+(define-key org-mode-map "\C-cs" my-sched-map)
 
 ;; ----------------------------------------
 ;; initialization
+
 (my-sched--load-maybe)
