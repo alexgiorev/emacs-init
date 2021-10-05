509f1d45355717d7a00388e6e478648696792c36
alexgiorev <alex.giorev@gmail.com>
2021-10-05 14:15:53 +0300

    my-org-vars-insert-link

--- my-org-vars/my-org-vars.el
+++ my-org-vars/my-org-vars.el
@@ -1,5 +1,5 @@
-(defvar my-org-vars--file
-  (concat user-emacs-directory "my-org-vars/my-org-vars--file")
+(defvar my-org-vars-file
+  (concat user-emacs-directory "my-org-vars/my-org-vars-alist")
   "The path to the file which stores the bindings")
 
 (defvar my-org-vars-alist nil
@@ -7,15 +7,23 @@
 
 ;; * persistence
 
-(defun my-org-vars--load nil
+(defun my-org-vars-load nil
   (let (buffer)
-    (with-current-buffer (setq buffer (find-file-noselect my-org-vars--file))
+    (with-current-buffer (setq buffer (find-file-noselect my-org-vars-file))
       (ignore-error 'end-of-file (setq my-org-vars-alist (read buffer)))
     (kill-buffer buffer))))
 
-(defun my-org-vars--flush nil
-  (with-temp-file my-org-vars--file
+(defun my-org-vars-flush nil
+  (with-temp-file my-org-vars-file
     (prin1 my-org-vars-alist (current-buffer))))
+
+;; * utils
+
+(defsubst my-org-vars-read-var nil
+  (completing-read "Identifier: " (mapcar 'car my-org-vars-alist)))
+
+(defsubst my-org-vars-get (name)
+  (cdr (assoc name my-org-vars-alist)))
 
 ;; * commands
 
@@ -26,25 +34,30 @@
     (if (setq binding (assoc name my-org-vars-alist))
         (setcdr binding id)
       (push (cons name id) my-org-vars-alist)))
-  (my-org-vars--flush))
+  (my-org-vars-flush))
 
 (defun my-org-vars-goto (name)
   "Jumps to the node bound to NAME. Signals `void-variable' error when NAME is
 not bound to any node."
-  (interactive (list (completing-read
-                      "Identifier: " (mapcar 'car my-org-vars-alist))))
-  (let ((id (cdr (assoc name my-org-vars-alist))))
+  (interactive (list (my-org-vars-read-var)))
+  (let ((id (my-org-vars-get name)))
     (org-id-open id nil)))
+
+(defun my-org-vars-insert-link (name)
+  (interactive (list (my-org-vars-read-var)))
+  (let ((id (my-org-vars-get name)))
+    (insert (org-link-make-string (concat "id:" id) name))))
 
 ;; * keymap
 
 (defvar my-org-vars-map (make-sparse-keymap))
 (progn
   (define-key my-org-vars-map "g" 'my-org-vars-goto)
-  (define-key my-org-vars-map "s" 'my-org-vars-set))
-(define-key org-mode-map "\C-cv" 'my-org-vars-map)
+  (define-key my-org-vars-map "s" 'my-org-vars-set)
+  (define-key my-org-vars-map "l" 'my-org-vars-insert-link))
+(define-key org-mode-map "\C-cv" my-org-vars-map)
 
 ;; * initialization
 
-(my-org-vars--load)
+(my-org-vars-load)
 (provide 'my-org-vars)
