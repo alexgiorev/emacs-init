51b1558803b10d39a75b2c5a2674cce44dd34492
alexgiorev <alex.giorev@gmail.com>
2021-10-07 14:35:40 +0300

    removed magit section

--- code.el
+++ code.el
@@ -14,23 +14,3 @@
 (with-eval-after-load 'prog-mode
   (define-key prog-mode-map
     (kbd "C-c M-w") 'my-kill-code-remove-indent))
-
-;; ----------------------------------------
-;; magit
-
-(defun my-magit-commit-file (&optional ammend)
-  "Stage the current file and commit. For convenience, attempts to put in the
-kill ring the name of the defun at point."
-  (interactive)
-  (ignore-errors
-    (kill-new (my-get-defun-name)))
-  (magit-stage-file (buffer-file-name))
-  (if ammend (magit-commit-amend) (magit-commit)))
-
-(defvar my-magit-map (make-sparse-keymap))
-(progn
-  (define-key my-magit-map "c" 'my-magit-commit-file)
-  (define-key my-magit-map "a"
-    (lambda nil (interactive) (my-magit-commit-file :amend)))
-  (define-key my-magit-map "p" 'magit-push-current-to-upstream))
-(define-key prog-mode-map "\C-cg" my-magit-map)
