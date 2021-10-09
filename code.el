5c61cb56380e16900da04e32a2efe1c68510f1e5
alexgiorev <alex.giorev@gmail.com>
2021-10-09 12:49:07 +0300

    my-elisp-isearch-defun

--- code.el
+++ code.el
@@ -14,3 +14,20 @@
 (with-eval-after-load 'prog-mode
   (define-key prog-mode-map
     (kbd "C-c M-w") 'my-kill-code-remove-indent))
+
+;; ----------------------------------------
+(defconst my-elisp-defun-re
+  "([[:space:]\n]*defun[[:space:]\n]+\\(\\(?:\\sw\\|\\s_\\)+\\)")
+
+(defun my-elisp-isearch-defun nil
+  (interactive)
+  (let ((isearch-filter-predicate
+         (lambda (beg end)
+           (save-match-data
+             (save-excursion
+               (beginning-of-line)
+               (and (looking-at my-elisp-defun-re)
+                    (<= (match-beginning 1) beg end (match-end 1))))))))
+    (isearch-mode :forward nil nil t)))
+
+(define-key emacs-lisp-mode-map (kbd "M-s d") 'my-elisp-isearch-defun)
