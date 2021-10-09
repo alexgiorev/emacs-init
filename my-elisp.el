ce0a12480fb56f42ff97a685a32fc596b81c235a
alexgiorev <alex.giorev@gmail.com>
2021-10-10 00:31:20 +0300

    my-elisp-defun-name

--- my-elisp.el
+++ my-elisp.el
@@ -16,8 +16,36 @@
 
 (define-key emacs-lisp-mode-map (kbd "M-s d") 'my-elisp-isearch-defun)
 
+(defun my-elisp-defun-name nil
+  (save-excursion
+    (beginning-of-line)
+    (unless (looking-at my-elisp-defun-re)
+      (beginning-of-defun))
+    (let (beg end)
+      (forward-symbol 2) (setq end (point))
+      (forward-symbol -1) (setq start (point))
+      (buffer-substring-no-properties start end))))
+
+(defun my-elisp-save-defun-name nil
+  "Puts in the kill ring the name of the current defun. Useful when taking notes
+about the function."
+  (interactive)
+  (kill-new (my-elisp-defun-name)))
+
+(with-eval-after-load 'elisp-mode
+  (define-key emacs-lisp-mode-map (kbd "C-c C-t")
+    'my-elisp-save-defun-name))
+
 ;; ----------------------------------------
 (define-key emacs-lisp-mode-map (kbd "C-c t")
   (lambda nil
     (interactive)
     (insert "(error \"TODO\")")))
+
+;; ----------------------------------------
+;; cpath
+(with-eval-after-load 'cpath
+  (add-hook 'emacs-lisp-mode-hook
+            (lambda nil
+              (setq cpath-func-name-func
+                    'my-elisp-defun-name))))
